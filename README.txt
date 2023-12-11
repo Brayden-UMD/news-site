8bf0746ba682116ddc7cc969ce078baa8b211728
root <root@LAPTOP-OG6QD3M6>
2023-12-10 20:20:50 -0500

    added pages env and get_requests.js

---
+++ README.txt
@@ -0,0 +1,8 @@
+Team Members - Aakash Pamnani ()
+Add the name and directory id (in parentheses) of each member of your team. If you are working by yourself, add your name and directory id too.
+
+App Description - One or two line description.
+
+API links - Links to API(s) you are using.
+
+YouTube Demo Video - Link to a YouTube video that provides a demo of your application. This video is very important. We will not grade a project unless a video is provided.
--- get_requests.js
+++ get_requests.js
@@ -5,12 +5,29 @@ const MAIN = require('./main.js');
 const express = require('express');
 const path = require('path');
 
+// Added
+const bodyParser = require("body-parser");
+
+require("dotenv").config({ path: path.resolve(__dirname, '.env') })  
+
+const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.smmpxbp.mongodb.net/?retryWrites=true&w=majority`
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+ /* Our database and collection */
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
+
 /*===========================================
             GET Route Handlers
 ===========================================*/
 
 function initializeGETHandlers() {
     MAIN.APP.set('views', path.join(__dirname, '/pages')); //All of our templates will be located in the "pages" folder
+
+    //Added
+    MAIN.APP.use(bodyParser.urlencoded({extended:false}));
+
+    /* view/templating engine */
+    MAIN.APP.set("view engine", "ejs");
 
     //Serve all static files from the "assets" folder
     MAIN.APP.use("/assets", express.static(path.join(__dirname, '/pages/assets')));
@@ -30,6 +47,75 @@ function initializeGETHandlers() {
             <%- text %>
         */
     });
+
+    //Serve Feedback Form
+    MAIN.APP.get('/review', (request, response) => {
+        response.render('submitFeedback');
+    });
+
+    //Feedback confirmation
+    MAIN.APP.post("/processFeedback", async (request, response) => {
+        const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+    
+        try {
+            await client.connect();
+            let date = new Date();
+    
+            /* Inserting application */
+            const feedback = {
+                name: request.body.name, 
+                email: request.body.email, 
+                review: request.body.review, 
+                timeCompleted: date.toDateString()
+            };
+            await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(feedback);
+            
+            response.render("processFeedback", feedback);
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    });
+
+    // Display Review List
+    MAIN.APP.get("/reviewList", async (request, response) => {
+        const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+        let reviewTable = '';
+    
+        try {
+            await client.connect();
+    
+            let filter = {};
+            const cursor = client.db(databaseAndCollection.db)
+            .collection(databaseAndCollection.collection)
+            .find(filter);
+    
+            const result = await cursor.toArray();
+    
+            if (result.length > 0) {
+                result.forEach(element => {
+                    reviewTable += `<tr><td>${element.name}</td><td>${element.email}</td><td>${element.review}</td><td>${element.timeCompleted}</td></tr>`; 
+                });
+                const reviewList = {
+                    numberReviews: result.length,
+                    reviewTable: reviewTable
+                };
+                response.render("displayReview", reviewList);
+            } else {
+                const reviewList = {
+                    numberReviews: 0,
+                    reviewTable: reviewTable
+                };
+                response.render("displayReview", reviewList);
+            }      
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    });
+
 }
 
 module.exports = { initializeGETHandlers };
